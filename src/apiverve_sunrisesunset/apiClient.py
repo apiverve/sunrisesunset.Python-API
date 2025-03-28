67aa07a5ac670e31d2efb84202d06489ca5a48b5
APIVerve Team <hello@apiverve.com>
2025-03-28 17:54:24 -0500

    Bug Fixes

--- src/apiverve_sunrisesunset/apiClient.py
+++ src/apiverve_sunrisesunset/apiClient.py
@@ -10,10 +10,10 @@ class SunrisesunsetAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/sunrisesunset'
+        self.base_url = 'https://api.apiverve.com/v1/sunrisesunset'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
